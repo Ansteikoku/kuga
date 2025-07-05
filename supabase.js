507d35cfcfaf315463a417882042b3411af81021
Ansteikoku <tianzhongxiaoji75@gmail.com>
2025-07-05 19:56:29 +0900

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = "https://hfyavvbummqkalugzago.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhmeWF2dmJ1bW1xa2FsdWd6YWdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE3MDc1MzAsImV4cCI6MjA2NzI4MzUzMH0.mB9Cgl0HZWItw2ItXeFuZAMP8vjXrpGc8juHgB-jfY0";
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
